61e41851c3d79016a0f1401a0075a37e4e6f4a39
Alexander Baron <alexander@baron-online.eu>
2021-10-14 10:24:50 +0200

    Setup

---
+++ docusaurus.config.js
@@ -0,0 +1,89 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "RWTH Notizen",
+  tagline: "Meine Notizen zum B.o.Sc. Informatik an der RWTH Aachen",
+  url: "https://notes.alexanders.site",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Alexander Baron", // Usually your GitHub org/user name.
+  projectName: "notes", // Usually your repo name.
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/Schwefelhexa/notes/edit/main/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/Schwefelhexa/notes/edit/main/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Informatik Notizen",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            href: "https://github.com/Schwefelhexa/notes",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Pflichtfächer",
+            items: [
+              {
+                label: "Berechenbarkeit und Komplexität",
+                to: "/docs/buk",
+              },
+            ],
+          },
+          {
+            title: "Anwendungsfach Medizin",
+            items: [
+              {
+                label: "Medizin 1",
+                to: "/docs/med1",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Alexander Baron. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
